808e948813b3fe80c691ec66d5d3a6c3590df487
Lee <natalia@timvw.co.uk>
2023-10-30 15:13:43 +0000

    WIP

--- app.py
+++ app.py
@@ -1,11 +1,19 @@
-from flask import Flask
+from flask import Flask, render_template, request
 import os
 
 app = Flask(__name__)
+upload_folder = "uploads"
 
-@app.route('/')
+@app.route('/index')
 def hello_world():
-    return "hello world"
+    return render_template('index.html')
+
+@app.route('/uploads', methods=["POST"])
+def upload_image():
+    file = request.files["image"]
+    os.makedirs(upload_folder, exist_ok=True)
+    file.save(os.path.join(upload_folder, file.filename))
+    return "Upload successful."
 
 if __name__ == '__main__':
     app.run(debug=True, host = "0.0.0.0")
